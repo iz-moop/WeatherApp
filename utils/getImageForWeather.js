9e108a8960b9ed36e4a4395d7857925edf1ad3cf
Ismath <ismathim@gmail.com>
2021-03-10 07:01:53 +0400

    updated android

--- utils/getImageForWeather.js
+++ utils/getImageForWeather.js
@@ -1,16 +1,16 @@
 /* eslint-disable global-require */
 
 const images = {
-  Clear: require('../assets/clear.png'),
-  Hail: require('../assets/hail.png'),
-  'Heavy Cloud': require('../assets/heavy-cloud.png'),
-  'Light Cloud': require('../assets/light-cloud.png'),
-  'Heavy Rain': require('../assets/heavy-rain.png'),
-  'Light Rain': require('../assets/light-rain.png'),
-  Showers: require('../assets/showers.png'),
-  Sleet: require('../assets/sleet.png'),
-  Snow: require('../assets/snow.png'),
-  Thunder: require('../assets/thunder.png'),
+  Clear: require('../assets/clear.jpg'),
+  Hail: require('../assets/hail.jpg'),
+  'Heavy Cloud': require('../assets/heavy-cloud.jpg'),
+  'Light Cloud': require('../assets/light-cloud.jpg'),
+  'Heavy Rain': require('../assets/heavy-rain.jpg'),
+  'Light Rain': require('../assets/light-rain.jpg'),
+  Showers: require('../assets/showers.jpg'),
+  Sleet: require('../assets/sleet.jpg'),
+  Snow: require('../assets/snow.jpg'),
+  Thunder: require('../assets/thunder.jpg'),
 };
 
 export default weather => images[weather];
